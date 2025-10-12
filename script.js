0ed5f399bc5299fee6c95b1172801011a32b1d5e
rpfg <83571951+rpfg@users.noreply.github.com>
2025-10-13 00:34:31 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
     <nav class="navbar">
       <div class="logo-container">
         <a href="/index.html" class="logo">
-          <img src="BBCS2/images/BBCLOGO.png" alt="BBC Logo" class="logo-img">
+          <img src="/BBCS2/images/BBCLOGO.png" alt="BBC Logo" class="logo-img">
         </a>
         <span class="logo-message">BBC Cavite Laguna NDR S2</span>
       </div>
@@ -12,10 +12,10 @@ document.addEventListener("DOMContentLoaded", () => {
       <label for="toggler"><i class="ri-menu-line"></i></label>
       <div class="menu">
         <ul class="list">
-          <li><a href="BBCS2/index.html">Home</a></li>
-          <li><a href="BBCS2/Funrace/fr.html">Funrace</a></li>
-          <li><a href="BBCS2/Derby/derby.html">Derby</a></li>
-          <li><a href="BBCS2/Patibayan/pat.html">Patibayan</a></li>
+          <li><a href="/BBCS2/index.html">Home</a></li>
+          <li><a href="/BBCS2/Funrace/fr.html">Funrace</a></li>
+          <li><a href="/BBCS2/Derby/derby.html">Derby</a></li>
+          <li><a href="/BBCS2/Patibayan/pat.html">Patibayan</a></li>
         </ul>
       </div>
     </nav>
