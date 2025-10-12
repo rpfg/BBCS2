d3475507c60371cd9c0926644e0e410280b542a2
rpfg <83571951+rpfg@users.noreply.github.com>
2025-10-13 00:45:41 +0800

    Update derby.js

--- Derby/derby.js
+++ Derby/derby.js
@@ -74,7 +74,7 @@ function buildRaceTable(values, table) {
   const headerIndex = values.findIndex(row => row[0] === "Rank");
   if (headerIndex === -1) return;
 
-  const headerRow = values[headerIndex].slice(0,12);
+  const headerRow = values[headerIndex].slice(0,14);
   const theadEl = document.createElement("thead");
   const trHead = document.createElement("tr");
   headerRow.forEach(h => {
